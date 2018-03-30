b89c79d70a16966bbf7d438a8bcdc8d610819df3
Forrest Jadick <forrest@baypointautomation.com>
2018-03-30 12:45:11 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,3 @@
 module.exports = {
-    remoteUrl : 'mongodb://remoteadmin:remoteadmin@ec2-54-83-164-158.compute-1.amazonaws.com:27017/dummyDB',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    remoteUrl : 'mongodb://remoteadmin:remoteadmin@ec2-54-83-164-158.compute-1.amazonaws.com:27017/dummyDB'
 };
